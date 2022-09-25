8fe718ab3a662be492d3ea9de83674a3341c8389
mark <mr.karimi1377@gmail.com>
2022-09-25 13:53:44 +0330

    add calculate map bounds function

---
+++ src/utils/calculateMapBounds.ts
@@ -0,0 +1,33 @@
+import { IProperty } from "../db/property.schema";
+
+type Bounds = {
+  topLeft: number[];
+  bottomRight: number[];
+};
+
+export default function CalculateMapBound(propertyList: IProperty[]): Bounds {
+  var minLat = Number.POSITIVE_INFINITY,
+    minLng = Number.POSITIVE_INFINITY;
+  var maxLat = Number.NEGATIVE_INFINITY,
+    maxLng = Number.NEGATIVE_INFINITY;
+
+  propertyList.forEach((property) => {
+    if (Number(property.lat) < minLat) {
+      minLat = Number(property.lat);
+    }
+    if (Number(property.lat) > maxLat) {
+      maxLat = Number(property.lat);
+    }
+    if (Number(property.long) < minLng) {
+      minLng = Number(property.long);
+    }
+    if (Number(property.long) > maxLng) {
+      maxLng = Number(property.long);
+    }
+  });
+
+  return {
+    topLeft: [minLat, maxLng],
+    bottomRight: [maxLat, minLng],
+  };
+}
